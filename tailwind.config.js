5c0d3426dd4a4a49fffd1b8df22e95620c92cee4
Morgan Livingston <101152345+MorgLivingston@users.noreply.github.com>
2022-12-19 18:38:18 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    fontSize: {
+     '6vw' : '6vw',
+     '7vw' : '7vw'
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
