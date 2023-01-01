7f14a1d025dbfba18124ffd4ec9c49aeb31a718d
Morgan Livingston <101152345+MorgLivingston@users.noreply.github.com>
2022-12-31 19:30:31 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,15 @@ module.exports = {
   content: ["*.html"],
   theme: {
     fontSize: {
+      '1vw' : '1vw',
+      '2vw' : '2vw',
+      '3vw' : '3vw',
+      '4vw' : '4vw',
+      '5vw' : '5vw',
      '6vw' : '6vw',
-     '7vw' : '7vw'
+     '7vw' : '7vw',
+     '8vw' : '8vw',
+     '9vw' : '9vw'
     },
 
     extend: {},
